c1eb61fac5fe7ba5b5c567739e64542b703fd95a
Aavart Sharma <rockstarfan0369@gmail.com>
2025-07-16 23:30:37 +0530

    worked on table_schema

--- database.py
+++ database.py
@@ -40,8 +40,35 @@ class PyDatabase():
         self.security = SecurityManager()
         self.db_path = Config.DATABASE_DIR / "base.db" 
         self.conn = self._initialize_database()
+        self.cursor = None
 
-        self._execute_query_admin("""   
+        #-------- lambda function --------#
+
+        self._delete_table = lambda tablename: _execute_query(f"DELETE FROM {tablename}")
+        self._drop_table = lambda tablename: _execute_query(f"DROP TABLE {tablename}")
+        self._drop_all = lambda drop_all: _execute_query("""SELECT 'DROP TABLE IF EXISTS "' || name || '";' FROM sqlite_master WHERE type='table' AND nam;w :e NOT LIKE 'sqlite_%';""")
+
+
+    # def delete_table(self,tablename:str):
+    #     _execute_query(f"DELETE FROM {tablename}")
+
+    # def drop_table(self,tablename:str):
+    #     _execute_query(f"DROP TABLE {tablename}")
+
+    # def drop_all(self):
+    #     self.conn.cursor().execute("""SELECT 'DROP TABLE IF EXISTS "' || name || '";' FROM sqlite_master WHERE type='table' AND nam;w :e NOT LIKE 'sqlite_%';""")
+
+    def _initialize_database(self) -> sqlite3.Connection:
+        """Initialize SQLite database with encryption"""
+        # Create database directory if it doesn't exist
+        # Config.DATABASE_DIR.mkdir(exist_ok=True)
+        Config.init()
+        
+        # Create new database connection
+        conn = sqlite3.connect(str(self.db_path))
+        conn.row_factory = sqlite3.Row
+        
+        self._execute_query(conn,"""   
             CREATE TABLE IF NOT EXISTS query_log (
                 Sno INTEGER PRIMARY KEY AUTOINCREMENT,
                 Query TEXT,
@@ -51,7 +78,7 @@ class PyDatabase():
             )
         """)
 
-        self._execute_query_admin("""
+        self._execute_query(conn,"""
             CREATE TABLE IF NOT EXISTS client (
                 Id TEXT PRIMARY KEY,
                 Name TEXT,
@@ -63,7 +90,7 @@ class PyDatabase():
             )
         """) 
 
-        self._execute_query_admin("""
+        self._execute_query(conn,"""
             CREATE TABLE IF NOT EXISTS table_owner (
                 Table_Id TEXT PRIMARY KEY,
                 Table_Name TEXT,
@@ -72,7 +99,7 @@ class PyDatabase():
             ) 
         """)
 
-        self._execute_query_admin("""
+        self._execute_query(conn,"""
             CREATE TABLE IF NOT EXISTS client_log (
                 Log_Id TEXT PRIMARY KEY,
                 Client_Id TEXT,
@@ -81,97 +108,94 @@ class PyDatabase():
                 logged_Out_At TEXT
             )
         """)
-        self.conn.commit()
-
-    def _initialize_database(self) -> sqlite3.Connection:
-        """Initialize SQLite database with encryption"""
-        # Create database directory if it doesn't exist
-        Config.DATABASE_DIR.mkdir(exist_ok=True)
-        
-        # Create new database connection
-        conn = sqlite3.connect(str(self.db_path))
-        conn.row_factory = sqlite3.Row
-        # print("row factory : ",conn.row_factory)
-        # Initialize query logging table and for now this should be changed
-        # there should be table crearte ion on intalixing
-        
         return conn
-
     
-    def _log_query(self, query: str, user: str, status: str = status.success) -> None:
-        """Log SQL query execution"""
-        try:
-            # print(_insert_query)
-            self._insert_query("query_log",Query=query,Timestamp=datetime.now().isoformat(" "),Client=user,Status = status)
-
-        except Exception as e:
-           logging.error(f"Failed to log query: {e}")
-           raise e
-    
-    def _insert_query(self,table_name: str, **column) -> status:
-        try:
-            self.conn.execute(
-                f"""INSERT INTO {table_name} ({",".join([i for i in column])}) VALUES ({",".join(["?" for i in range(len(column))])})""",
-                tuple(column[i] for i in column)
-            )
-            self.conn.commit()
-            return status.success
-        except Exception as e:
-            logging.error(f"Failed to log Query: {e}")
-            raise e
-            return status.failed
-
-
-    def _execute_query(self, user: str,query: str, params: Optional[tuple] = None) -> Dict[str, Any]:
-        """Execute a SQL query with security checks"""
-        try:
-            cursor = self.conn.cursor()
+    def _execute_query(self,conn,query: str, params: tuple | None = None) -> Dict: # here is no sql injection security
+        try: 
+            cursor = conn.cursor()
             if params:
                 cursor.execute(query, params)
             else:
                 cursor.execute(query)
-            self.conn.commit()
-            logging.info(cursor.description)
+            conn.commit()
             columns = lambda descrp: [description[0] for description in descrp] if descrp else None
             results = [dict(zip(columns(cursor.description), row)) for row in cursor.fetchall()]
-            self._log_query(query, user)
             return {
                 "columns": columns(cursor.description),
                 "rows": results,
                 "row_count": len(results),
                 "rows_affected": cursor.rowcount
             }
-                
         except Exception as e:
-            self._log_query(query, user, status.failed)
+            # self._log_query(query,"admin",status.failed)
+            logging.error(f"Error excuting {query},{params}: {e}")
             raise e
     
-    def _execute_query_admin(self,query: str, parmas: tuple | None = None) -> Dict: # here is no sql injection security
-        try: 
-            self.conn.cursor().execute(query)
-            self.conn.commit()
-            # logging.info(f"Query is {query}")
-            self._log_query(query,"admin",status.success)
+    def _log_query(self, query: str, user: str, status: str = status.success) -> None:
+        """Log SQL query execution"""
+        try:
+            # print(_insert_query)
+            self._insert_query("aavart",table_name="query_log",Query=query,Timestamp=datetime.now().isoformat(" "),Client=user,Status = status)
+
         except Exception as e:
-            self._log_query(query,"admin",status.failed)
+           logging.error(f"Failed to log query: {e}")
+           raise e
+    
+    def _insert_query(self,user: str,table_name: str, **column) -> status:
+        """asdfasdfasdfasdfsafsadf"""
+        try:
+            query= f"""INSERT INTO {table_name} ({",".join([i for i in column])}) VALUES ({",".join(["?" for i in range(len(column))])})"""
+            params= tuple(column[i] for i in column)
+            self._execute_query(self.conn,query,params)
+            return status.success
+        except Exception as e:
+            logging.error(f"Failed to log Query: {e}")
             raise e
+            return status.failed
+
+    def _create_table():
+        pass
+
+    def _table_schema():
+        pass
+
+    # def _insert_query(self,table_name: str, **column) -> status:
+    #     """dsfsadfasfasdfasfdsaf"""
+    #     try:
+    #         self.conn.execute(
+    #             f"""INSERT INTO {table_name} ({",".join([i for i in column])}) VALUES ({",".join(["?" for i in range(len(column))])})""",
+    #             tuple(column[i] for i in column)
+    #         )
+    #         self.conn.commit()
+    #         return status.success
+    #     except Exception as e:
+    #         logging.error(f"Failed to log Query: {e}")
+    #         raise e
+    #         return status.failed
+
+
+    # def _execute_query(self, user: str,query: str, params: Optional[tuple] = None) -> Dict[str, Any]:
+    #     """Execute a SQL query with security checks"""
+    #     _execute_query(self.conn,query,params)
     
 
     def table_schema(self, table_name: str) -> List[Dict[str, str]]:
         """Get schema information for a table"""
-        cursor = self.conn.cursor()
-        cursor.execute(f"PRAGMA table_info({table_name})")
+        rows = self._execute_query(self.conn,f"PRAGMA table_info({table_name});")
+        logging.info(f"log: {rows}")
+        logging.info(f"cursor: {self.conn.cursor().fetchall()}")
+        # return self.conn.cursor().fetchall()
         return [
             {
                 "name": row[1],
                 "type": row[2],
                 "notnull": bool(row[3]),
-                "pk": bool(row[5])
+                "pk": bool(row[5]),
             }
-            for row in cursor.fetchall()
+            for row in self.conn.cursor().fetchall()
         ]
 
-    def insert_query(self,table_name: str, **column) -> status:
+    def insert(self,table_name: str, **column) -> status:
         try:
             self._insert_query(table_name,**column)
 
@@ -182,7 +206,6 @@ class PyDatabase():
             raise e
             return status.failed
     
-
     def create_table(self, table_name: str, user: str = "system", *columns: List[Column]) -> Dict[str, Any]:
         """Create a new table with specified columns"""
         # Validate table name (prevent SQL injection)
@@ -207,28 +230,12 @@ class PyDatabase():
             logging.error(f"Error occured: {e}")
             raise e
 
+
     def verify_token(self, client_token: str) -> bool:
         query = f"""select * from client where Id='{client_token}'"""
         _execute_query("system",)
         pass
 
-    def delete_table(self,tablename:str):
-        self.conn.cursor().execute(f"DELETE FROM {tablename}")
-        self.conn.commit()
-        pass
-
-    def drop_table(self,tablename:str):
-        self.conn.cursor().execute(f"DROP TABLE {tablename}")
-        self.conn.commit()
-        pass
-
-    def drop_all(self):
-        self.conn.cursor().execute("""SELECT 'DROP TABLE IF EXISTS "' || name || '";'
-FROM sqlite_master
-WHERE type='table' AND nam;w
-                                   :e NOT LIKE 'sqlite_%';""")
-        self.conn.commit()
-        pass
                                  
 if (__name__ == "__main__"):  # for test componett of this file
     db= PyDatabase()
