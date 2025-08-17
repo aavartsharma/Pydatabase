66745581123eff49f79b77da056f3b041742f923
dev sharma <rockstarfan0369@gmail.com>
2025-08-17 22:50:24 +0530

    written the archture

--- database.py
+++ database.py
@@ -8,7 +8,7 @@ from pydantic import BaseModel # for datetime
 from security import SecurityManager   # security.py
 from sqlmodel import Field as field, Session, SQLModel, create_engine, select
 from sqlalchemy.schema import CreateTable
-from sqlalchmey import inspect, Select
+from sqlalchemy import inspect, Select,text
 from typing import Any, Dict, List, Optional, Union, TypeVar   # for type annotation
 
 logging = logger.Utility(name=__file__,version=Config.version,detail="idnotknow").logger #(__name__,Config.version,"Idon'tknow",Config.project_name)
@@ -20,7 +20,7 @@ class status(Enum):
 
 
 class Hero(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
+    id: int | None = field(default=None, primary_key=True)
     name: str
     secret_name: str
     age: int | None = None
@@ -34,224 +34,43 @@ class StaticMethodMeta(type):
             new_dct[key] = value
         return super().__new__(cls,name, bases, new_dct)
 
-class PyDatabase_client():
-    # add singleton pattern 
-    def __init__(self):
-        self.security = SecurityManager()
-        self.engine = create_engine(f"sqlite:///{Config.DATABASE_MAIN}")
-        # self.cursor = self.conn.cursor()  self._delete("test", id >3 and name = aavart sharma or class = 3 or iq=230)
-
-        #-------- Private lambda function --------#
-        self._delete = lambda self, table_name, condition: self._execute_query(f"DELETE FROM {table_name} WHERE {str(condition)}")
-        self._delete_all = lambda self,table_name:self. _execute_query(f"DELETE FROM {table_name}")
-        self._drop_table = lambda self,table_name: self._execute_query(f"DROP TABLE {table_name}")
-        self._drop_all = lambda self: self._execute_query("""SELECT 'DROP TABLE IF EXISTS "' || name || '";'
-FROM sqlite_master
-WHERE type = 'table' AND name NOT LIKE 'sqlite_%'""")
-
-# -------------------- Private functions -------------------- #
-
-    def _initialize_database(self) -> sqlite3.Connection:
-        """Initialize SQLite database with encryption"""
-        # Create database directory if it doesn't exist
-        # Config.DATABASE_DIR.mkdir(exist_ok=True)
-        Config.init()
-        
-        SQLModel.metadata.create_all(self.engine)
-
-        # Create new database connection
-        conn = sqlite3.connect(str(self.db_path))
-        conn.row_factory = sqlite3.Row
-        cursor = conn.cursor()
-        self._execute_query("""   
-            CREATE TABLE IF NOT EXISTS query_log (
-                Sno INTEGER PRIMARY KEY AUTOINCREMENT,
-                Query TEXT,
-                Timestamp TEXT,
-                Client TEXT,
-                Status TEXT
-            )
-        """,conn=conn,cursor=cursor)
-        self._execute_query("""
-            CREATE TABLE IF NOT EXISTS client (
-                Id TEXT PRIMARY KEY,
-                Name TEXT,
-                Token TEXT,
-                Joined TEXT,
-                Active TEXT,
-                Owned_Tables TEXT,
-                File_Location TEXT
-            )
-        """,conn=conn,cursor=cursor) 
-
-        self._execute_query("""
-            CREATE TABLE IF NOT EXISTS table_owner (
-                Table_Id TEXT PRIMARY KEY,
-                Table_Name TEXT,
-                Owner_Id TEXT,
-                Owner_name TEXT
-            ) 
-        """,conn=conn,cursor=cursor)
-
-        self._execute_query("""
-            CREATE TABLE IF NOT EXISTS client_log (
-                Log_Id TEXT PRIMARY KEY,
-                Client_Id TEXT,
-                Client_Name TEXT,
-                logged_In_At TEXT,
-                logged_Out_At TEXT
-            )
-        """,conn=conn,cursor=cursor)
-        return conn , cursor
-    
-    def _execute_query(self,query: str, params: tuple | None = None,conn=None,cursor=None) -> Dict: # here is no sql injection security
-        try:
-            if not conn:
-                conn = self.conn
-            if not cursor:
-                cursor = self.cursor
-            if params:
-                cursor.execute(query, params)
-            else:
-                cursor.execute(query)
-            conn.commit()
-            columns = lambda descrp: [description[0] for description in descrp] if descrp else None
-            results = [dict(zip(columns(cursor.description), row)) for row in cursor.fetchall()]
-            return {
-                "columns": columns(cursor.description),
-                "rows": results,
-                "row_count": len(results),
-                "rows_affected": cursor.rowcount
-            }
-        except Exception as e:
-            # self._log_query(query,"admin",status.failed)
-            logging.error(f"Error excuting {query},{params}: {e}")
-            raise e
-    
-    def _log_query(self, query: str, user: str, status: str = status.success) -> None:
-        """Log SQL query execution"""
-        try:
-            # print(_insert_query)
-            self._insert_query("aavart",table_name="query_log",Query=query,Timestamp=datetime.now().isoformat(" "),Client=user,Status = status)
-
-        except Exception as e:
-           logging.error(f"Failed to log query: {e}")
-           raise e
-    
-    def _insert_query(self,table_name: str, **column) -> status:
-        """asdfasdfasdfasdfsafsadf"""
-        try:
-            query= f"""INSERT INTO {table_name} ({",".join([i for i in column])}) VALUES ({",".join(["?" for i in range(len(column))])})"""
-            params= tuple(column[i] for i in column)
-            self._execute_query(query,params)
-            return status.success
-        except Exception as e:
-            logging.error(f"Failed to log Query: {e}")
-            raise e
-            return status.failed
-
-    def _create_table(self, table_name: str, *columns: List[Column]):
-        query = f"""
-        CREATE TABLE IF NOT EXISTS {table_name} (
-            {', '.join(tuple(i.querystr() for i in columns))}
-        )
-        """
-
-        try:
-            result = self._execute_query(query)
-            return result , query
-
-        except Exception as e:
-            logging.info(locals())
-            logging.error(f"Error occured: {e}")
-            return e , query
-
-    def _table_schema(self,table_name:str):
-        rows = self._execute_query(f"PRAGMA table_info({table_name});")
-        return rows
-        # return [
-        #     {
-        #         "name": row[1],
-        #         "type": row[2],
-        #         "notnull": bool(row[3]),
-        #         "pk": bool(row[5]),
-        #     }
-        #     for row in rows
-        # ]
-
-    def _fetch(self,table_name: str, condition: SQLExpr = None, *columns:List[str]) -> List[Dict[str,any]]:
-        col = "*" if not columns else ",".join([i for i in columns]) 
-        if(not condition):
-            return self._execute_query(f"SELECT {col} from {table_name}")
-        logging.info(f"SELECT {col} from {table_name} WHERE {str(condition)}")
-        return self._execute_query(f"SELECT {col} from {table_name} WHERE {str(condition)}")
-        
-# -------------------- Public function -------------------- #
-
-    def fetch(self, user, table_name, condition: SQLExpr = None, *columns: List[str]): # fsadfsd
-        return self._fetch(table_name, condition,*columns)
-
-    def table_schema(self, user, table_name: str) -> List[Dict[str, str]]:
-        """Get schema information for a table"""
-        logging.info(f"User query - user:{user}, queryed: {self.table_schema.__name__}")
-        return self._table_schema(table_name)
-
-    def insert(self, user,table_name: str, **column) -> status:
-        try:
-            for i in column:
-                if keyword.iskeyword(i):
-                    raise ValueError(f"arg can't be a keyword , {i}")
-            self._insert_query(table_name,**column)
-
-        except sqlite3.OperationalError as e:
-            logging.warning(f"Schema of {table_name} is {self._table_schema(table_name)}")
-        except Exception as e:
-            logging.error(f"Failed to log Query: {e}")
-            raise e
-            return status.failed
-    
-    def create_table(self, user: str, table_name: str, *columns: List[Column]) -> Dict[str, Any]:
-        """Create a new table with specified columns"""
-        # Validate table name (prevent SQL injection)
-        for i in columns:
-            if keyword.iskeyword(i.name):
-                raise ValueError(f"arg can't be a keyword , {i.name}")
-        result, query = self._create_table(table_name,*columns)
-        logging.info(f"Create table's Query: {query}")
-        result["status"] = status.success
-        return result
-    
-    def delete(self, user: str,table_name: str, condition: SQLExpr = None) -> Any:
-        if not condition:
-            return self._delete_all(table_name)
-        return self._delete(table_name, condition)
-
-    def drop_table(self, user: str, table_name: str= None):
-        if( not table_name):
-            return self._drop_all()
-        return self._drop_table(table_name)
-
-    def verify_token(self, client_token: str) -> bool:
-        query = f"""select * from client where Id='{client_token}'"""
-        self._execute_query("system",)
-        pass
 
 class Pydatabase():
     def __init__(self):
         self.security = SecurityManager()
-        self.engine = create_engine(f"sqlite:///{Config.DATABASE_MAIN}")
+        self.engine = create_engine(f"sqlite:///{Config.DATABASE_MAIN}",echo=True)
+        self.inspector = inspect(self.engine)
 
     def _initialize_database(self):
         Config.init()
         class query_log(SQLModel, table=True):
-            pass
+            Sno: Opitonal[int] = field(primary_key=True)
+            Query: str
+            Time_Stamp: str
+            Client: str
+            Status: str
 
         class client(SQLModel, table=True):
-            pass
+            Id: str = field(primary_key=True)
+            Name: str
+            Token: str
+            Joined: str
+            Active: str
+            Owned_tables: str
+            File_location: str
 
-        class table_owner(SQLModel, table=True): pass
+        class table_owner(SQLModel, table=True):
+            Table_Id: str = field(primary_key=True)
+            Table_Name: str
+            Owner_Id: str
+            Owner_Name: str
 
-        class client_log(SQLModel, table=True): pass
+        class client_log(SQLModel, table=True):
+            Id: str = field(primary_key=True)
+            Client_Id : str
+            Client_Name: str
+            Logged_In_At: str
+            Logged_Out_At: str
 
         # query_log
         # client
@@ -280,13 +99,11 @@ class Pydatabase():
     def table_schema(self, table_name: str):
         return self.inspector.get_columns(table_name)
 
-    def insert(self, cliebt_name: str, rows: List[T]) -> Status:
+    def insert(self, cliebt_name: str, rows: List[T]) -> status:
         with Session(self.engine) as session:
             for i in rows:
                 session.add(i)
             session.commit()
-            for i in rows:
-                session.refreash(i)
             pass
 
     def create_table(self, table_class):  # maek a system later
@@ -299,6 +116,11 @@ class Pydatabase():
             session.exec(statement)
             session.commit()
         pass
+
+    def alter_table(self):
+        with engine.connect() as conn:
+            conn.execute(text("ALTER TABLE user ADD COLUMN email VARCHAR"))
+            conn.commit()
 
     def delete_all(self):
         pass
@@ -315,9 +137,16 @@ class Pydatabase():
 
 # -------------------- TEST AREA -------------------- #                               
 if (__name__ == "__main__"):  # for test componett of this file
-    db= PyDatabase()
+    db= Pydatabase()
 
-    
+    class test(SQLModel, table=True):
+        id: Optional[int] = field(primary_key=True)
+        name: str
+
+    SQLModel.metadata.create_all(db.engine)
+    rows = [test(id=1,name="aavar"), test(id=2,name="asf")]
+    db.insert("sfsfsf", rows)
+    # db.insert("agsag", )
     pass
 
 
